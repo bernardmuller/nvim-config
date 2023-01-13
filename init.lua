4523a11bc6181e4755b62e27985c3270e4f43bf5
Bernard Muller <b.mullerjnr@gmail.com>
2023-01-13 12:08:49 +0200

    restructure;

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- Install packer
+require 'bernard.keymaps'
+
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
