a0046fa36bdcfee21b9c454e5f42a996367d1920
Bernard Muller <b.mullerjnr@gmail.com>
2024-02-03 17:46:24 +0200

    feat: add copilot

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -90,11 +90,22 @@ local plugins = {
       return M
     end
   },
+
   -- DAP
   {
     "mfussenegger/nvim-dap"
   },
 
+  -- Copilot
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function ()
+     vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    end
+  }
 }
 
 return plugins
